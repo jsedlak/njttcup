15b8408361875d923b42ce0998ea1f3247d00c88
John Sedlak <john.sedlak@outlook.com>
2024-09-25 23:08:49 -0400

    Adds MainNav component Fixes MainNav active status Removes unnecessary ICategoryWindow pattern. Data updates can come from the top through event loopback.

--- src/CupKeeper.AdminApp/Components/Windows/ICategoryWindow.cs
+++
@@ -1,8 +0,0 @@
-using CupKeeper.Domains.Championships.ViewModel;
-
-namespace CupKeeper.AdminApp.Components.Windows;
-
-public interface ICategoryWindow
-{
-    Task HandleCategoryUpdated(CategoryResultViewModel category);
-}
